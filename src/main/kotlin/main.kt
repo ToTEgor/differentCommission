bc4cd7467b2b1c3401f4fa46e474169922a9391e
Egor <ooonaliv@mail.ru>
2023-04-01 18:36:23 +0300

    corrected

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,26 @@
 fun main() {
-    val tax = trans("Visa", amount = 1200)
-    println("Скидка $tax")
 
-}
-fun trans(cardName : String = "VK pay", lastTransfer : Int = 0, amount : Int) = when (cardName) {
-    "Mastercard", "Maestro" -> if (amount > 75_000) amount / 100 * 0.6 + 20 else 0.0
-    "Visa", "Мир" -> peace(amount)
-    else -> 0.0
+    println(trans("Maestro",0,133000))
 }
 
-fun peace (amount: Int) : Double{
-    var tax = amount / 100 * 0.75
-    if(tax <= 35) return 35.0 else return tax
+fun trans(cardName: String = "VK pay", lastTransfer: Int = 0, amount: Int) = when (cardName) {
+    "Mastercard", "Maestro" -> when {
+        lastTransfer > 150_000 || lastTransfer + amount > 600_000 -> println("Превышен лимит перевода")
+        lastTransfer in 300..75000 -> 0
+        else -> (amount * 0.006).toInt() + 20
+    }
+    "Visa", "Мир" -> when {
+        lastTransfer > 150_000 || lastTransfer + amount > 600_000 -> println("Превышен лимит перевода")
+        else -> peace(amount)
+    }
+    "VK pay" -> when {
+        lastTransfer > 15_000 || lastTransfer + amount > 40_000 -> println("Превышен лимит перевода")
+        else -> 0
+    }
+    else -> println("Таким способом перевод невозможен")
+}
+
+fun peace(amount: Int): Double {
+    var tax = amount * 0.0075
+    if (tax <= 35) return 35.0 else return tax
 }
